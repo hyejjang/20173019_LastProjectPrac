ef1f5dada60b49cf1103d1c23fd7563136857c1b
hyejj <hyejjang26@naver.com>
2021-11-27 20:34:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MovieListResult.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+import java.util.ArrayList;
+import android.graphics.Movie;
+
+public class MovieListResult {
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+}
